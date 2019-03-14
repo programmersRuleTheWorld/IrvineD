649557019303ab043da3f45dcbe125af7b17d053
Robbie <programmers.rule.the.world@gmail.com>
2019-03-13 19:58:56 -0500

    4th

--- bs4/src/components/homepagecards.php
+++ bs4/src/components/homepagecards.php
@@ -1,6 +1,6 @@
 <div class="row">
 
-  <div class="card text-dark text-center bg-warning col-lg-3 col-md-8 col-sm-9 ml-auto mr-auto mb-2" style="width: 18rem;">
+  <div class="card text-dark text-center bg-warning col-lg col-md-8 col-sm-9 ml-auto mr-auto mb-2" style="width: 18rem;">
     <img class="card-img-top" src="src/assets/webdev.jpeg" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Web/App Development/Repair</h5>
@@ -8,7 +8,7 @@
         You want your site or app to look good, but more importantly you want it to make you money; we're going to help get you there.
     </div>
   </div>
-  <div class="card text-dark text-center bg-warning col-lg-3 col-md-8 col-sm-11 ml-auto mr-auto mb-2" style="width: 18rem;">
+  <div class="card text-dark text-center bg-warning col-lg col-md-8 col-sm-9 ml-auto mr-auto mb-2" style="width: 18rem;">
     <img class="card-img-top" src="src/assets/machinelearning.jpeg" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Automation</h5>
@@ -16,7 +16,7 @@
         With cutting-edge machine-learning techniques, we can not only cut your payroll, but increase your earnings. Tell us what you want and we'll see if it can be done.
     </div>
   </div>
-  <div class="card text-dark text-center bg-warning col-lg-3 col-md-8 col-sm-9 ml-auto mr-auto mb-2" style="width: 18rem;">
+  <div class="card text-dark text-center bg-warning col-lg col-md-8 col-sm-9 ml-auto mr-auto mb-2" style="width: 18rem;">
     <img class="card-img-top" src="src/assets/pentest.jpg" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Security Testing</h5>
