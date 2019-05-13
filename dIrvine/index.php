344c039d1da0878bfac7dad8391c9149cb958b6d
Robbie <programmers.rule.the.world@gmail.com>
2019-05-12 19:33:07 -0500

    Final Beta

---
+++ dIrvine/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="css/styles.css">
+<link rel="stylesheet" href="../node_modules/bootstrap-social/bootstrap-social.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body>
+<div class="container-fluid">
+<div class="row">
+<div class="col-6 m-auto mt-4">
+<a href="https://www.facebook.com/profile.php?id=100013443635638"><i class="fa fa-facebook-official" style="font-size:48px;color:blue"></i></a>
+<a href="https://twitter.com/p9akoil"><i class="fa fa-twitter-square" style="font-size:48px;color:cyan"></i></a>
+<a href="https://www.instagram.com/thatsuckingsound/"><i class="fa fa-instagram" style="font-size:48px;color:red"></i></a>
+<strong>Connect with us!</strong>
+</div>
+</div>
+</div>
+</body>
+</html>
